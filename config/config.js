94753861e1257ec658fc4d100e57ad7e1d721ea5
talat <talatm02@gmail.com>
2019-07-31 01:12:44 +0400

    post change

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "loglevel": "all",
     "showconsole": true,
-    "port": 3000,
+    "port": process.env.PORT || 3000,
     "auth": {
         "user": process.env.OPENJET_USER,
         "pass": process.env.OPENJET_PASSWORD
